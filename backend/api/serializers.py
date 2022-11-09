a64893a7e92627deb7dee4116341544c4fc013ea
SoheilAlimadadi <103267081+SoheilAlimadadi@users.noreply.github.com>
2022-11-09 18:32:00 +0330

    pagination and changes to seializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -3,9 +3,23 @@ from products.models import Product
 from rest_framework.reverse import reverse
 from .validators import unique_product_title
 
+class UserOtherProductInlineSerializer(serializers.Serializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name =  'product-detail',
+        lookup_field = 'pk',
+        read_only = True
+    )
+    title = serializers.CharField(read_only=True)
+
 class UserPublicSerializer(serializers.Serializer):
     username = serializers.CharField(read_only=True)
     id = serializers.CharField(read_only=True)
+    other_product = serializers.SerializerMethodField(read_only=True)
+    
+    def get_other_product(self, obj):
+        user = obj
+        my_product_qs = user.product_set.all()[:5]
+        return UserOtherProductInlineSerializer(my_product_qs, context=self.context, many=True).data
     
 class ProductSerializer(serializers.ModelSerializer):
     owner = UserPublicSerializer(source='user', read_only=True)
